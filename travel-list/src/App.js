4578634191bbd57f41bc4494078b5d6a2e472028
Kartik Shetty <kartik15613@mastek.com>
2023-07-11 17:43:42 +0530

    Derived stats for footer

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -112,10 +112,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.ceil((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go"
+          : `👜You have ${numItems} items on your list, and you already packed
+          ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
