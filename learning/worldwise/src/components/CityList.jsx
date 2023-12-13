0228fb8bdaccc99b3b223c412a1e1a27b603a2d7
Kartik Shetty <kartik15613@mastek.com>
2023-12-13 18:43:51 +0530

    asd

--- learning/worldwise/src/components/CityList.jsx
+++ learning/worldwise/src/components/CityList.jsx
@@ -3,7 +3,11 @@ import Message from "./Message";
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 
-function CityList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
